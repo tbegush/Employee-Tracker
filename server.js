6507aabf90e04816235645ae308545f27bddee5c
Thomas Begush <tbegush@gmail.com>
2021-08-02 18:25:12 -0700

    questions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const db = require("./db/connection");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+const cTable = require("console.table");
+const { query } = require("./db/connection");
 
 // function for the main menu
 const mainMenu = () => {
@@ -42,51 +43,127 @@ const mainMenu = () => {
         ],
       },
     ])
-    .then(({answer}) => { 
-         console.log(mainMenu)
-      if ((answer === "VAD")) {
+    .then(({ answer }) => {
+      // console.log(mainMenu)
+      if (answer === "VAD") {
         db.query("SELECT id, name FROM department", function (err, res) {
-            if (err) throw err;
-            console.table(res);
+          if (err) throw err;
+          console.table(res);
         });
-       } 
-      //else if ((mainMenu.list === "VAR")) {
-      //   db.query("SELECT * FROM role", function (err, res) {
-      //       if (err) throw err;
-      //       console.table(res);
-      //   });
-      // } else if ((mainMenu.list === "VAE")) {
-      //   console.log("VAE SUCCESS")
-      //   db.query("SELECT * FROM employee", function (err, res) {
-      //       if (err) throw err;
-      //       console.table(res);
-      //   });
-      // }else if ((mainMenu.list === "AAD")) {
-      //   db.query("                  ", function (err, res) {
-      //       if (err) throw err;
-      //       console.table(res);
-      //   });
-      // } else if ((mainMenu.list === "AAR")) {
-      //   db.query("                  ", function (err, res) {
-      //       if (err) throw err;
-      //       console.table(res);
-      //   });
-      // }else if ((mainMenu.list === "AAE")) {
-      //   db.query("                   ", function (err, res) {
-      //       if (err) throw err;
-      //       console.table(res);
-      //   });
-      // }else if ((mainMenu.list === "UAER")) {
-      //   db.query("                       ", function (err, res) {
-      //       if (err) throw err;
-      //       console.table(res);
-      //   });
-      // }
+      } else if (answer === "VAR") {
+        db.query("SELECT * FROM role", function (err, res) {
+          if (err) throw err;
+          console.table(res);
+        });
+      } else if (answer === "VAE") {
+        console.log("VAE SUCCESS");
+        db.query("SELECT * FROM employee", function (err, res) {
+          if (err) throw err;
+          console.table(res);
+        });
+      } else if (answer === "AAD") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "department",
+              message: "Enter new Department name:",
+            },
+          ])
+          .then(({ department }) => {
+            db.query(
+              `INSERT INTO department (name) VALUES ("${department}")`,
+              function (err, res) {
+                if (err) throw err;
+                console.log(`Added new ${department} department`);
+              }
+            );
+          });
+      } else if (answer === "AAR") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "role",
+              message: "Enter new Role name",
+            },
+            {
+              type: "input",
+              name: "salary",
+              message: "Enter new salary",
+            },
+            {
+              type: "input",
+              name: "department",
+              message: "Enter dept",
+            },
+          ])
+          .then(({ role, salary, department }) => {
+            db.query(
+              `INSERT INTO role (title,salary,department_id) VALUES ("${role}",${salary},${department})`,
+              function (err, res) {
+                if (err) throw err;
+                console.log(`Added new ${role} role`);
+              }
+            );
+          });
+      } else if (answer === "AAE") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "first_name",
+              message: "Enter first name",
+            },
+            {
+              type: "input",
+              name: "last_name",
+              message: "Enter last name",
+            },
+            {
+              type: "input",
+              name: "role",
+              message: "Enter employee's role",
+            },
+            {
+              type: "input",
+              name: "manager",
+              message: "Enter manager name",
+            },
+          ])
+          .then(({ first_name, last_name, role, manager }) => {
+            db.query(
+              `INSERT INTO employee (first_name,last_name, role_id, manager_id) VALUES ("${first_name}","${last_name}",${role},${manager})`,
+              function (err, res) {
+                if (err) throw err;
+                console.log(`Added new employee ${first_name}`);
+              }
+            );
+          });
+      } else if (answer === "UAER") {
+        let employees = [];
+        let roles = [];
 
-
-
-
-    
+        db.query(
+          "Select id as value, concat(first_name,' ',last_name) as name FROM employee",
+          function (err, res) {
+            if (err) throw err;
+            employees = res;
+            db.query("select id, title FROM role", function (err, res) {
+              if (err) throw err;
+              roles = res;
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'employee',
+                  message: 'Select an employee to update.',
+                  choices: employees
+                },
+              ]).then((selectedEmp))
+            });
+          }
+        );
+      }
     });
 };
 
